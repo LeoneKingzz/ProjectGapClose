a798d704aa6b2fe957eaa663e8e72f5b026f8f44
LeoneKingzz <167722980+LeoneKingzz@users.noreply.github.com>
2024-10-02 13:02:10 +0100

    Update hook.cpp

--- src/hook.cpp
+++ src/hook.cpp
@@ -555,7 +555,7 @@ namespace hooks
 	void OnMeleeHitHook::Update(RE::Actor* a_actor, [[maybe_unused]] float a_delta)
 	{
 		if (a_actor->GetActorRuntimeData().currentProcess && a_actor->GetActorRuntimeData().currentProcess->InHighProcess() && a_actor->Is3DLoaded()){
-			logger::info("Update Hook is working. Processing updates...")
+			logger::info("Update Hook is working. Processing updates...");
 			logger::info("Name {} info {}"sv, a_actor->GetName(), "update recieved");
 			auto bPGC_IsInCombat = false;
 			if ((a_actor->GetGraphVariableBool("bPGC_IsInCombat", bPGC_IsInCombat) && bPGC_IsInCombat) && is_melee(a_actor)) {
